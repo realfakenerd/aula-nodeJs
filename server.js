ad8004080b215dadc7c9d8a115053219a07c3bbc
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2019-07-09 15:16:48 -0300

    refactor of the project

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 const http = require('http');
+const app= require('express');
 
-const servidor = http.createServer(function (req, resp) {
-    resp.end(`
-            <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-            </html>
-    `)
+app.listen(3000, function () {
+    console.log('Escutando na porta 3000');
 });
-servidor.listen(3000);
