3c6df0e8f3292c1436d46e5a1d80455cad32545a
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2019-07-09 15:20:39 -0300

    added app.get to the server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const http = require('http');
-const app= require('express');
+const app = require('express');
 
 app.listen(3000, function () {
     console.log('Escutando na porta 3000');
 });
+
+app.get('/', function (req, resp) {
+    resp.send(`
+    <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1> Casa do Código </h1>
+    </body> 
+    </html>
+`)
+})
