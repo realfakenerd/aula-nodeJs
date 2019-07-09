d449af0cea89c70a5827ac4111ce65cd3ce54ea0
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2019-07-09 19:24:10 -0300

    added new methods

--- src/app/infra/BooksDao.js
+++ src/app/infra/BooksDao.js
@@ -33,8 +33,70 @@ class BooksDao {
             this._db.all(
                 'SELECT * FROM livros',
                 (err, result) => {
-                    if (err) return reject('Não foi possível os livros');
+                    if (err) return reject('Não foi possível listar os livros');
                     return resolve(result);
+                }
+            )
+        });
+    }
+
+    searchForId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (err, livro) => {
+                    if (err) {
+                        return reject('Não foi possível encontrar o livro!')
+                    }
+                    return resolve(livro);
+                }
+            )
+        });
+    }
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    UPDATE livros SET
+                    titulo = ?,
+                    preco = ?,
+                    descricao = ?
+                    WHERE id = ?
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao,
+                    livro.id
+                ],
+                err => {
+                    if (err) {
+                        return reject('Não foi possível atualizar o livro')
+                    }
+                    resolve();
+                }
+            )
+        });
+    }
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                DELETE 
+                FROM livros
+                WHERE id = ?
+                `,
+                [id],
+                err => {
+                    if (err) {
+                        return reject('Não foi possível remover o livro')
+                    }
+                    return resolve()
                 }
             )
         });
